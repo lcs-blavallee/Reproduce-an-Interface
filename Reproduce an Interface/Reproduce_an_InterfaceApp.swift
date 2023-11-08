382fdf98373f157696921bf55653cddb2d367c5c
Benjamin Lavallee <blavallee@lcs.on.ca>
2023-11-08 08:56:22 -0500

    Initial Commit

---
+++ Reproduce an Interface/Reproduce_an_InterfaceApp.swift
@@ -0,0 +1,17 @@
+//
+//  Reproduce_an_InterfaceApp.swift
+//  Reproduce an Interface
+//
+//  Created by Benjamin Lavallee on 2023-11-08.
+//
+
+import SwiftUI
+
+@main
+struct Reproduce_an_InterfaceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
